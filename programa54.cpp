f5dcbd44278e66ca25d084cf979ce99dd7736b37
GARCIA PEREZ DANNA PAOLA <168700889+DANNAPAOOO@users.noreply.github.com>
2024-06-10 10:49:12 -0600

    Create programa54.cpp

---
+++ programa54.cpp
@@ -0,0 +1,9 @@
+ // Inciso e:Danna paola Verificar si el número está entre 50 y 100
+    if (numero_entero > 50 && numero_entero < 100) {
+        std::cout << "e) " << numero_entero << " es mayor que 50 y menor que 100." << std::endl;
+    } else {
+        std::cout << "e) " << numero_entero << " no cumple con la condición de ser mayor que 50 y menor que 100." << std::endl;
+    }
+
+    return 0;
+}
